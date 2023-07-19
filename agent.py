bc477cbaf8b760903fa5bb952390ce4927bfb1cf
suzanaSSP <117389578+suzanaSSP@users.noreply.github.com>
2023-07-19 17:34:04 -0600

    Added predator

--- agent.py
+++ agent.py
@@ -13,16 +13,19 @@ So now, they just go to the eating state if the switch is True, and the switch o
 
 class Buffalo:
     dt = 0.1
-    
-    def __init__(self, x, y, leader=None):
+    speed = 5
+    def __init__(self, x, y, size_area):
         self.x = x
         self.y = y
         self.c = np.array([self.x, self.y])
-        # self.v = np.array([self.speed * math.cos(math.pi/2), self.speed*math.sin(math.pi/2)])
 
-        self.state = "hungry"
+        self.theta  = np.random.uniform(-np.pi, np.pi) 
+        self.v = np.array([np.cos(self.theta), np.sin(self.theta)])
+
+        self.state = "grouping"
         self.change_x = 0
         self.change_y = 0
+        self.change_theta = 0
 
         self.grass_eating = None
         self.satisfaction = random.randint(80, 100)
@@ -30,25 +33,30 @@ class Buffalo:
         
         self.switch = False 
 
-        self.leader = self if leader is None else leader
+        self.size_area = size_area
+        self.size = 50
 
-    def check_state(self):
-        if self.switch:
+    def check_state(self, predator):
+        if math.dist(predator.c, self.c) < 100:
+            self.state = "run_away_from_predator"
+        elif self.switch:
             self.state = "eating"
         elif self.grass_eating or self.satisfaction < 40:
             self.state = "hungry"
         else:
-            self.state = "wandering"
+            self.state = "grouping"
 
-    def perform_action(self, grasses):   
-        self.check_state()
+    def perform_action(self, grasses, predator):   
+        self.check_state(predator)
         
-        if self.state == "wandering":
-            self.go_to_center()
+        if self.state == "grouping":
+            self.grounping()
         elif self.state == "hungry":
             self.find_food(grasses)
         elif self.state == "eating":
             self.eat()
+        elif self.state == "run_away_from_predator":
+            self.avoid_predator(predator)
         
     def move(self):
         new_x = self.x + (self.change_x * self.dt) 
@@ -91,55 +99,87 @@ class Buffalo:
         return closest_grass
 
     def eat(self):
-        if self.grass_eating and self not in self.grass_eating.agents_feeding:
-            self.grass_eating.agents_feeding.append(self)  
+        if self.grass_eating:
+            if self not in self.grass_eating.agents_feeding:
+                self.grass_eating.agents_feeding.append(self)  
               
-        self.satisfaction += 1
+            self.satisfaction += 1
         
-        if self.satisfaction == 100 and self.grass_eating:
-            self.grass_eating.agents_feeding.remove(self)
-            self.grass_eating = None
+            # If the grass has been eaten buffalo should go look for another grass if it's still hungry
+            if not self.grass_eating.switch and self.switch:
+                self.state = "hungry"
+                self.grass_eating.agents_feeding.remove(self)
+                self.grass_eating = None
+
+            if self.satisfaction == 100:
+                self.grass_eating.agents_feeding.remove(self)
+                self.grass_eating = None
+        else:    
             self.switch = False
 
-    def go_to_center(self):
-        new_location = [random.randint(20, 40), random.randint(20, 40)]
+    def grounping(self):
+        new_location = [random.randint(0, self.size_area//4), random.randint(0, self.size_area//4)]
         self.change_x = new_location[0] - self.x
         self.change_y = new_location[1] - self.y 
         
         self.satisfaction -= 1
 
+    def avoid_predator(self, predator):
+        # repulsion_factor = -1 * ((predator.c - self.c)/ np.linalg.norm(predator.c - self.c)**2)
+        self.change_x = self.x - predator.x
+        self.change_y = self.y - predator.y
 
 
-class Buffalo_Leader(Buffalo):
+class Predator(Buffalo):
+    def __init__(self, x, y, size_area, leader=None):
+        super().__init__(x, y, size_area)
+        self.clock = 0
+        self.state = "chase_buffalo"
+        self.color = (1, 0.5, 0) # Orange
+        self.size = 100
 
-    screen_width = 600
-    screen_height = 600
-    
-    def __init__(self, x, y, leader=None):
-        super().__init__(x, y)
+    def check_state(self):
+        if self.clock > 20:
+            self.state = "chase_buffalo"
+        else:
+            self.state = "wait"
 
-    # def check_grass(self, grasses):
-    #     # grass_list = grasses
-    #     # for grass in grass_list:
-    #     #     if math.dist(self.c, grass.c) <= 5:
-    #     #         grass_list.remove(grass)
-                
-    #     # chosen_grass = grass_list[0]
-    #     self.change_x = chosen_grass.x - self.x
-   
-    #     self.satisfaction -= 1
-    
-            
+    def perform_action(self, buffalos):
+        # self.check_state()
+
+        if self.state == "chase_buffalo":
+            self.chase_buffalo(buffalos)
+        else:
+            self.clock += 1
+
+    def chase_buffalo(self, buffalos):
+        attraction_factor = 0
+
+        for buffalo in buffalos[1:]:
+            if math.dist(buffalo.c, self.c) < 500:
+                attraction_factor += (buffalo.c - self.c)
         
-    # def perform_action(self, grasses):
-    #     self.check_state()
-        
-    #     if self.state == "follow_the_leader":
-    #         self.check_grass(grasses)
-    #     elif self.state == "hungry":
-    #         self.find_food(grasses)
-    #     else:
-    #         self.eat()
+        # # No buffalos are near, need to move forward
+        # if all(attraction_factor) == 0:
+        #     self.change_x = self.x + self.speed*self.clock
+        #     self.change_y = self.y + self.speed*self.clock
+        # else:
+        if not math.isnan(attraction_factor):
+            Ua = attraction_factor / np.linalg.norm(attraction_factor)       
+            w = 0.5 * (np.arctan2(Ua[1], Ua[0]) - self.theta)
 
+            self.change_x = (self.speed * self.v)
+            self.theta = self.theta + (self.dt * w)
+            self.v = np.array([np.cos(self.theta), np.sin(self.theta)])
+ 
+        self.clock += 1
+
+        # Time is up, change states
+        if self.clock == 150:
+            self.clock = 0
+            self.c = [0,0]
+
+
+        
 
 
--- images/main.py
+++ images/main.py
@@ -1,6 +1,6 @@
 from world import Grass
 import random
-from agent import Buffalo, Predator
+from agent import Buffalo, Buffalo_Leader
 import matplotlib.pyplot as plt
 import math
 
@@ -10,17 +10,16 @@ min_value = 250
 max_value = 750
 Nt = 1000
 num_grass = 5
-num_buffalos = 2
+num_buffalos = 20
 
 def main():
     fig, ax = plt.subplots()
 
-    # leader = Buffalo_Leader(random.randint(min_value, max_value), random.randint(min_value, max_value), L)
-    predator = Predator(0, 0, L)
-    buffalos = [Buffalo(random.randint(min_value, max_value), random.randint(min_value, max_value), L) for _ in range(num_buffalos)]
+    leader = Buffalo_Leader(random.randint(min_value, max_value), random.randint(min_value, max_value), L)
+    buffalos = [Buffalo(random.randint(min_value, max_value), random.randint(min_value, max_value), L,  leader) for _ in range(num_buffalos)]
     grasses = [Grass(random.randint(L//4, L), random.randint(L//4, L), random.randint(0,10)) for _ in range(num_grass)]
 
-    buffalos.insert(0, predator)
+    buffalos.insert(0, leader)
 
     for i in range(Nt):
         
@@ -28,16 +27,12 @@ def main():
             grass.perform_action()
         
         for buffalo in buffalos:
-            if isinstance(buffalo, Predator):
-                buffalo.perform_action(buffalos)
-            else:
-                buffalo.perform_action(grasses, predator)
+            buffalo.perform_action(grasses)
             buffalo.move()
         
         x = [buffalo.x for buffalo in buffalos]
         y = [buffalo.y for buffalo in buffalos]   
         c = [buffalo.color for buffalo in buffalos]
-        s = [buffalo.size for buffalo in buffalos]
         
         grass_x = [grass.x for grass in grasses]
         grass_y = [grass.y for grass in grasses]
@@ -47,7 +42,7 @@ def main():
         if plotRealTime or (i == Nt-1):
             plt.cla()
             plt.scatter(grass_x, grass_y, s=grass_s, c=grass_c)
-            plt.scatter(x, y, s=s, c=c)
+            plt.scatter(x, y, s=50, c=c)
             ax.set(xlim=(0,L),ylim=(0,L))
             plt.pause(0.1)
         
@@ -55,7 +50,6 @@ def main():
         chosen_grass = grasses[0]
         chosen_buffalo = buffalos[0]
         print(chosen_buffalo.state)
-        print(chosen_buffalo.clock)
 
             
     plt.show()
