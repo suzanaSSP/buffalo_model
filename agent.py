54a31a575a107a5de183ec93201dd303bbb1391c
suzanaSSP <117389578+suzanaSSP@users.noreply.github.com>
2023-08-18 16:18:19 -0600

    Changed predator states

--- agent.py
+++ agent.py
@@ -11,22 +11,24 @@ class Buffalo:
         self.y = y
         self.c = np.array([self.x, self.y])
 
+        self.state = None
+
         self.theta  = np.random.uniform(-np.pi, np.pi) 
         self.v = np.array([np.cos(self.theta), np.sin(self.theta)])
-        self.speed = 7 if self.state == "run_away_from_predator" else 5
+        self.speed = 7 if self.state == 'run_away_from_predator' else 5 
 
-        self.state = "grouping"
         self.grass_eating = None
-        self.satisfaction = random.randint(150, 200)
+        self.satisfaction = random.randint(180, 200) 
         self.color = (0.4, 0.2, 0) # Brown (102, 51, 0)
-        self.neighbors = None
         self.switch = False 
+        
+        self.neighbors = None
         self.leader =  self if leader is None else leader
         self.predator = None if predator is None else predator
 
         self.new_theta = 0
-        self.new_v = None
-        self.new_c = None
+        self.new_v = np.array([0,0])
+        self.new_c = np.array([0,0])
        
     def movement(self, U):
         w = 0.5 * (np.arctan2(U[1], U[0]) - self.theta)
@@ -42,16 +44,20 @@ class Buffalo:
         self.v = self.new_v
         self.theta = self.new_theta
         
-        self.satisfaction -= 1
+        if self.state != "eating":
+            self.satisfaction -= 1
     
     def check_state(self, buffalos):
         self.neighbors = [other_buffalo for other_buffalo in buffalos if math.dist(other_buffalo.c, self.c) <= 200 and other_buffalo is not self]
         
         if self.predator in self.neighbors:
-            self.state = "run_away_from_predator"              
+            if len(self.neighbors) > 10 and math.dist(self.c, self.predator.c) < 100:
+                self.state == "attack_predator"
+            else:
+                self.state = "run_away_from_predator"             
         elif self.switch:
             self.state = "eating"
-        elif self.grass_eating or self.satisfaction < 40:
+        elif self.satisfaction < 100:
             self.state = "hungry"
         else:
             self.state = "grouping"
@@ -67,6 +73,8 @@ class Buffalo:
             self.eat()
         elif self.state == "run_away_from_predator":
             self.avoid_predator()
+        else:
+            self.attack_predator(buffalos)
         
     # HUNGRY STATE
     def find_food(self, grasses):
@@ -99,11 +107,11 @@ class Buffalo:
 
     # EATING STATE
     def eat(self):
+        self.satisfaction += 1
         if self.grass_eating:
             if self not in self.grass_eating.agents_feeding:
                 self.grass_eating.agents_feeding.append(self)  
               
-            self.satisfaction += 1
         
             # If the grass has been eaten buffalo should go look for another grass if it's still hungry
             if not self.grass_eating.switch and self.switch:
@@ -111,7 +119,7 @@ class Buffalo:
                 self.grass_eating.agents_feeding.remove(self)
                 self.grass_eating = None
 
-            if self.satisfaction == 100:
+            if self.satisfaction == 200:
                 self.grass_eating.agents_feeding.remove(self)
                 self.grass_eating = None
         else:    
@@ -137,6 +145,11 @@ class Buffalo:
         Ur = (-1 * ((self.predator.c - self.c)/ np.linalg.norm(self.predator.c - self.c)**2)) * 5
 
         self.movement(Ur)
+
+    # ATTACK_PREDATOR STATE
+    def attack_predator(self, buffalos):
+        buffalos.remove(self.predator)
+
         
 class BuffaloLeader(Buffalo):
     def __init__(self, x, y, leader=None):
@@ -154,40 +167,81 @@ class Predator(Buffalo):
     def __init__(self, x, y, leader=None, predator=None):
         super().__init__(x, y, leader=None, predator=None)
         self.color = (1, 0.5, 0) # Orange
-        self.speed = 5
+        self.speed = 6
+        self.buffalos_eaten = 0
+        self.satisfaction = 400
 
-    def perform_action(self, buffalos):
+    # def find_prev_state(self):
+    #     self.states.append(self.state)
+    #     if len(self.states) == 3:
+    #         del self.states[0]
+
+    #     if len(self.states) == 2:
+    #         self.prev_state = self.states[0]
+    #         self.state = self.states[1]
+
+
+    def check_states(self, buffalos):
         # Only bufalos that are in the predators radius of hunt are added in this list
         self.neighbors = [other_buffalo for other_buffalo in buffalos if math.dist(other_buffalo.c, self.c) < 500 and other_buffalo is not self]
-        self.neighbor_dist = [math.dist(agent.c, self.c) for agent in self.neighbors]
-        # If the list is not empty, you can hunt :)
-        if self.neighbors:
-            self.state = "chase_buffalo"
-            self.chase_buffalo(buffalos)
-        else:
-            self.state = "random_walk"
-            self.random_walk()
         
+        if self.satisfaction <= 300:
+            self.state = "chase_buffalos"
+        elif self.satisfaction > 300 and self.state != "hide":
+            self.state = "find_hiding_place"
+        elif self.state == "find_hiding_place" and not self.neighbors: # Found good place to hide with no buffalos nearby
+            self.state = "hide"
+            
+    
+    def perform_action(self, buffalos):
+        self.check_states(buffalos)
 
-    # WAITING STATE
+        if self.state == "find_hiding_place":
+            self.find_location()
+        elif self.state == "chase_buffalos":
+            self.chase_buffalo(buffalos)
+    
+    # HIDE STATE
+
+    # FIND HIDING PLACE STATE
+    def find_location(self):
+        repulsion_factor = np.array([0,0], dtype='float64')
+        for neighbor in self.neighbors:
+            repulsion_factor += (neighbor.c - self.c)/np.linalg.norm(neighbor.c - self.c)**2
+
+        Ur = -1 * repulsion_factor
+        self.movement(Ur)
     
     # CHASE_BUFFALO STATE
     def chase_buffalo(self, buffalos):
-        attraction_factor = 0
+        if not self.neighbors:
+            self.go_closer_to_buffalos(buffalos)
+        else:
+            attraction_factor = 0
 
-        for buffalo in self.neighbors:
-            if math.dist(buffalo.c, self.c) < 50:
-                self.satisfaction = 200
-                # buffalo.color = (0.5, 0.5, 0.5)
-                buffalos.remove(buffalo)
-            else:
-                attraction_factor += (buffalo.c - self.c)
+            for buffalo in self.neighbors:
+                if math.dist(buffalo.c, self.c) < 50:
+                    buffalos.remove(buffalo)
+                    self.buffalos_eaten += 1
+                else:
+                    attraction_factor += (buffalo.c - self.c)
+
+            self.satisfaction = self.buffalos_eaten * 100
+
+            Ua = attraction_factor / np.linalg.norm(attraction_factor)
+            self.movement(Ua)
+            
+    def go_closer_to_buffalos(self, buffalos):
+        attraction_factor = 0
+        for buffalo in buffalos:
+            attraction_factor += (buffalo.c - self.c)
 
         Ua = attraction_factor / np.linalg.norm(attraction_factor)
         self.movement(Ua)
-        
+
     # EXPLORING STATE
     def random_walk(self):
+        self.clock += 1
         self.new_theta = np.random.uniform(-(np.pi)/4, np.pi/2)
         self.new_v = np.array([np.cos(self.new_theta), np.sin(self.new_theta)])
         self.new_c = self.c + (self.speed * self.new_v * dt)
--- main.py
+++ main.py
@@ -16,17 +16,27 @@ predator = Predator(random.randint(min_value, max_value), random.randint(min_val
 buffalo_leader = BuffaloLeader(random.randint(min_value, max_value), random.randint(min_value, max_value))
 buffalos = [Buffalo(random.randint(min_value, max_value), random.randint(min_value, max_value), buffalo_leader, predator) for _ in range(num_buffalos)]
 grasses = [Grass(random.randint(L//4, L), random.randint(L//4, L), random.randint(0,10)) for _ in range(num_grass)]
-buffalos.extend([buffalo_leader, predator])
+
+buffalos.append(buffalo_leader)
+buffalos.append(predator)
+
 
 def perfom_agent_actions(buffalos, grasses):
     for grass in grasses:
             grass.perform_action()
         
     for buffalo in buffalos:
+
         if isinstance(buffalo, Predator):
-            buffalo.perform_action(buffalos)
+            if buffalo.satisfaction < -50:
+                buffalos.remove(buffalo)
+            else:
+                buffalo.perform_action(buffalos)
         else:
-            buffalo.perform_action(grasses, buffalos)
+            if buffalo.satisfaction < -50 or math.dist(buffalo.c, predator.c) < 50:
+                buffalos.remove(buffalo)
+            else:
+                buffalo.perform_action(grasses, buffalos)
 
 
 def main_simulation(buffalos, grasses):
@@ -41,16 +51,14 @@ def main_simulation(buffalos, grasses):
         for buffalo in buffalos:
             buffalo.update()
         
-        x = [buffalo.x % L for buffalo in buffalos]
-        y = [buffalo.y % L for buffalo in buffalos]   
+        x = [buffalo.x if buffalo.x <= L else buffalo.x - 100 for buffalo in buffalos]
+        y = [buffalo.y if buffalo.y <= L else buffalo.y - 100 for buffalo in buffalos]   
         c = [buffalo.color for buffalo in buffalos]
         
-        grass_x = [grass.x for grass in grasses]
-        grass_y = [grass.y for grass in grasses]
+        grass_x = [grass.x if grass.x <=L else grass.x - 100 for grass in grasses]
+        grass_y = [grass.y if grass.y <=L else grass.y - 100 for grass in grasses]
         grass_c = [grass.color for grass in grasses]
         grass_s = [grass.size for grass in grasses]
-        
-        radius_circle = plt.Circle((x[-1], y[-1]), 100)
 
         if plotRealTime or (i == Nt-1):
             plt.cla()
@@ -60,8 +68,10 @@ def main_simulation(buffalos, grasses):
             ax.set(xlim=(0,L),ylim=(0,L))
             plt.pause(0.1)
 
-        
-          
+            chosen_buffalo = buffalos[0]
+            print(chosen_buffalo.state)
+            print(chosen_buffalo.satisfaction)
+   
     plt.show()
 
 if __name__ == '__main__':
